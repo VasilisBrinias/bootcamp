70e0c41349660485328d2a69d0f4d060315f9eeb
VasilisBrinias <62116369+VasilisBrinias@users.noreply.github.com>
2020-03-12 21:26:30 +0200

    Add files via upload

---
+++ lion/lion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            double lion = 23000;
+            while(lion>6000)
+            {
+                lion = lion - (lion * 0.02);
+                counter++;
+            }
+            Console.WriteLine($"Lions will be extinct in {counter} years");
+        }
+    }
+}
